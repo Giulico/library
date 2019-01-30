7a2f0e4e6f5d2247ed3f698acff6d1b596ffc7f5
giuliocollesei <giuliocollesei@gmail.com>
2019-01-30 18:49:38 +0100

    First commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/giuliocollesei/repo/library/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/giuliocollesei/repo/library/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/Users/giuliocollesei/repo/library/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/Users/giuliocollesei/repo/library/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("/Users/giuliocollesei/repo/library/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+}
+
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/giuliocollesei/repo/library/.cache/data.json")
+
